e3cc90aff592b0fe09a630107f4ceec85aff3331
Paul Smith <paul.j.smith@ucl.ac.uk>
2022-11-10 16:30:54 +0000

    Add init files to packages and sub-packages

---
+++ MetricsReloaded/_version.py
@@ -0,0 +1,9 @@
+__appname__ = "MetricsReloaded"
+__version_info__ = (0, 1, 0)  # global application version, major/minor/patch
+__version__ = "%i.%i.%i" % __version_info__
+__description__ = "A new recommendation framework for biomedical image analysis validation"
+__author__ = "Carol Sudre"
+__author_email__ = "c.sudre@ucl.ac.uk"
+__copyright__ = (
+    "Copyright (c) 2022 Carole Sudre, all rights reserved. see LICENSE file."
+)
