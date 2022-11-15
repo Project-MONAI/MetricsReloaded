f4087498378c394419ac88204c4d47e47207eb52
Carole Sudre <csudre@Caroles-MBP.home>
2022-11-15 16:49:53 +0000

    Adding test for Average Precision

--- test/test_metrics/test_prob_pairwise_measures.py
+++ test/test_metrics/test_prob_pairwise_measures.py
@@ -6,7 +6,9 @@ import numpy as np
 from numpy.testing import assert_allclose
 from sklearn.metrics import cohen_kappa_score as cks
 from sklearn.metrics import matthews_corrcoef as mcc
-
+from sklearn.metrics import precision_recall_curve as prc
+from sklearn.metrics import average_precision_score as aps
+from MetricsReloaded.utility.utils import trapezoidal_integration
 from MetricsReloaded.metrics.prob_pairwise_measures import ProbabilityPairwiseMeasures
 
 
@@ -18,6 +20,22 @@ def test_auc():
     print(value_test)
     expected_auc = 0.78
     assert_allclose(value_test, expected_auc, atol=0.01)
+
+def test_ap():
+    ref = np.asarray([0,0,0,1,1,1])
+    pred_proba = np.asarray([0.21, 0.35, 0.63, 0.92, 0.32, 0.79])
+    ppm = ProbabilityPairwiseMeasures(pred_proba, ref)
+    threshs = [0, 0.21, 0.32, 0.35, 0.63, 0.79, 0.92]
+    recall = [1,1,0.66667,0.66667,0.66667,0.33,0]
+    prec =[0.5, 0.6, 0.5, 0.66667, 1, 1, 1]
+    expected_ap = trapezoidal_integration(np.asarray(recall)[::-1], np.asarray(prec)[::-1])
+    print("From SK", prc(ref, pred_proba))
+
+    expected_aps = aps(ref, pred_proba)
+    value_test = ppm.average_precision()
+    assert_allclose(value_test, expected_ap, atol=0.01)
+
+
 
 def test_sensitivity_at_specificity():
     ref = np.concatenate([np.zeros([50]), np.ones([50])])
