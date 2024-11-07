Adding formulas to calibration measures, correcting kernel calculation, creating test for confusion matrix

MetricsReloaded/metrics/calibration_measures.py
@@ -91,7 +91,7 @@ class CalibrationMeasures(object):
 
             cwECE = \dfrac{1}{K}\sum_{k=1}^{K}\sum_{i=1}^{N}\dfrac{\vert B_{i,k} \vert}{N} \left(y_{k}(B_{i,k}) - p_{k}(B_{i,k})\right)
 
-
+        :return: cwece
         """
 
         if "bins_ece" in self.dict_args:
@@ -139,6 +139,13 @@ class CalibrationMeasures(object):
         Derives the expectation calibration error in the case of binary task
         bins_ece is the key in the dictionary for the number of bins to consider
         Default is 10
+
+        .. math::
+
+            ECE = \sum_{m=1}^{M} \dfrac{|B_m|}{n}(\dfrac{1}{|B_m|}\sum_{i \in B_m}1(pred_ik==ref_ik)-\dfrac{1}{|B_m|}\sum_{i \in B_m}pred_i)
+
+        :return: ece
+
         """
         if "bins_ece" in self.dict_args:
             nbins = self.dict_args["bins_ece"]
@@ -179,22 +186,44 @@ class CalibrationMeasures(object):
         Glenn W Brier et al. 1950. Verification of forecasts expressed in terms of probability. Monthly weather review 78, 1
         (1950), 1–3.
 
+        .. math::
+
+            BS = \dfrac{1}{N}\sum_{i=1}{N}\sum_{j=1}^{C}(p_{ic}-r_{ic})^2
+
+        where :math: `p_{ic}` is the probability for class c and :math: `r_{ic}` the binary reference for class c and element i
+
         :return: brier score (BS)
+
         """
         bs = np.mean(np.sum(np.square(self.one_hot_ref - self.pred),1))
         return bs
 
     def root_brier_score(self):
         """
+        Determines the root brier score
+
         Gruber S. and Buettner F., Better Uncertainty Calibration via Proper Scores
         for Classification and Beyond, In Proceedings of the 36th International
         Conference on  Neural Information Processing Systems, 2022
+
+        .. math::
+
+            RBS = \sqrt{BS}
+
+        :return: rbs
         """
-        return np.sqrt(self.brier_score())
+        rbs = np.sqrt(self.brier_score())
+        return rbs
 
     def logarithmic_score(self):
         """
         Calculation of the logarithmic score https://en.wikipedia.org/wiki/Scoring_rule
+        
+        .. math::
+
+            LS = 1/N\sum_{i=1}^{N}\log{pred_ik}ref_{ik}
+
+        :return: ls
         """
         eps = 1e-10
         log_pred = np.log(self.pred + eps)
@@ -204,6 +233,11 @@ class CalibrationMeasures(object):
         return ls
 
     def distance_ij(self,i,j):
+        """
+        Determines the euclidean distance between two vectors of prediction for two samples i and j
+
+        :return: distance
+        """
         pred_i = self.pred[i,:]
         pred_j = self.pred[j,:]
         distance = np.sqrt(np.sum(np.square(pred_i - pred_j)))
@@ -211,20 +245,36 @@ class CalibrationMeasures(object):
 
 
     def kernel_calculation(self, i,j):
+        """
+        Defines the kernel value for two samples i and j with the following definition for k(x_i,x_j)
+
+        .. math::
+
+            k(x_i,x_j) = exp(-||x_i-y_j||/ \\nu)I_{N}
+
+        where :math: `\\nu` is the bandwith defined as the median heuristic if not specified in the options and N the number of classes
+
+        :return: kernel_value
+
+        """
         distance = self.distance_ij(i,j)
         if 'bandwidth_kce' in self.dict_args.keys():
             bandwidth = self.dict_args['bandwidth_kce']
         else:
             bandwidth = median_heuristic(self.pred)
         value = np.exp(-distance/bandwidth)
-        identity = np.ones([self.pred.shape[1], self.pred.shape[1]])
-        return value * identity
+        identity = np.eye(self.pred.shape[1])
+        kernel_value = value*identity
+        return kernel_value
 
     def kernel_calibration_error(self):
         """
         Based on the paper Widmann, D., Lindsten, F., and Zachariah, D.
         Calibration tests in multi-class classification: A unifying framework.
         Advances in Neural Information Processing Systems, 32:12257–12267, 2019.
+
+        :return: kce
+
         """
         one_hot_ref = one_hot_encode(self.ref, self.pred.shape[1])
         numb_samples = self.pred.shape[0]
@@ -246,6 +296,9 @@ class CalibrationMeasures(object):
         """
         Calculation of the top-label classification error. Assumes pred_proba a matrix K x Numb observations
         with probability to be in class k for observation i in position (k,i)
+
+        :return: tce
+
         """
         class_max = np.argmax(self.pred, 1)
         prob_pred_max = np.max(self.pred, 1)
@@ -271,7 +324,12 @@ class CalibrationMeasures(object):
         Teodora Popordanoska, Raphael Sayer, and Matthew B Blaschko. 2022. A Consistent and Differentiable Lp Canonical
         Calibration Error Estimator. In Advances in Neural Information Processing Systems.
 
+        .. math::
+ 
+            ECE\_KDE = 1/N \sum_{j=1}^{N}||\dfrac{\sum_{i \\neq j}k_{Dir}(pred_j,pred_i)ref_i}{\sum_{i \\neq j}k_{Dir}(pred_j,pred_i)} - pred_j || 
+
         :return: ece_kde
+
         """
         ece_kde = 0
         one_hot_ref = one_hot_encode(self.ref, self.pred.shape[1])
@@ -298,6 +356,18 @@ class CalibrationMeasures(object):
         return ece_kde
 
     def gamma_ik(self, i, k):
+        """
+        Definition of gamma value for sample i class k of the predictions
+
+        .. math::
+
+            gamma_{ik} = \Gamma(pred_{ik}/h + 1)
+
+        where h is the bandwidth value set as default to 0.5
+
+        :return gamma_ik
+
+        """
         pred_ik = self.pred[i, k]
         if "bandwidth" in self.dict_args.keys():
             h = self.dict_args["bandwidth"]
@@ -308,6 +378,16 @@ class CalibrationMeasures(object):
         return gamma_ik
 
     def dirichlet_kernel(self, j, i):
+        """
+        Calculation of Dirichlet kernel value for predictions of samples i and j
+
+        .. math::
+
+            k_{Dir}(x_j,x_i) = \dfrac{\Gamma(\sum_{k=1}^{K}\\alpha_{ik})}{\prod_{k=1}^{K}\\alpha_{ik}}\prod_{k=1}^{K}x_jk^{\\alpha_{ik}-1}
+        
+        :return: kernel_value
+
+        """
         pred_i = self.pred[i, :]
         pred_j = self.pred[j, :]
         nclasses = self.pred.shape[1]
@@ -334,7 +414,9 @@ class CalibrationMeasures(object):
 
         .. math::
 
-            -\sum_{i=1}{N} log(p_{i,k} | y_i=k)
+            NLL = -\sum_{i=1}{N} log(p_{i,k} | y_i=k)
+
+        :return: NLL
 
         """
         log_pred = np.log(self.pred)

MetricsReloaded/metrics/pairwise_measures.py
@@ -197,6 +197,7 @@ class MultiClassPairwiseMeasures(object):
         Determination of the expectation matrix to be used for CK derivation
 
         :return: expectation_matrix
+
         """
         one_hot_pred = one_hot_encode(self.pred, len(self.list_values))
         one_hot_ref = one_hot_encode(self.ref, len(self.list_values))
@@ -225,7 +226,7 @@ class MultiClassPairwiseMeasures(object):
         numerator = np.sum(weights * cm)
         denominator = np.sum(weights * exp)
         weighted_cohens_kappa = 1 - numerator / denominator
-        return 1 - numerator / denominator
+        return weighted_cohens_kappa
 
     def to_dict_meas(self, fmt="{:.4f}"):
         """Given the selected metrics provides a dictionary with relevant metrics"""
@@ -272,6 +273,7 @@ class BinaryPairwiseMeasures(object):
             "hd_perc": (self.measured_hausdorff_distance_perc, "HDPerc"),
             "masd": (self.measured_masd, "MASD"),
             "nsd": (self.normalised_surface_distance, "NSD"),
+            "avdr": (self.absolute_volume_difference_ratio, "AVDR")
         }
 
         self.pred = pred
@@ -481,7 +483,7 @@ class BinaryPairwiseMeasures(object):
 
         .. math::
 
-            Sens = \dfrac{TP}{\sharp Ref}
+            Sens = \dfrac{TP}{|Ref|}
 
         Yerushalmy J., Statistical Problems in assessing Methods of Medical Diagnosis with Special reference to X-Ray Techniques, 1947, Public Health Reports, pp1432-1449
 
@@ -501,7 +503,7 @@ class BinaryPairwiseMeasures(object):
 
         .. math::
 
-            Spec = \dfrac{TN}{\sharp {1-Ref}}
+            Spec = \dfrac{TN}{|1-Ref|}
 
         Yerushalmy J., Statistical Problems in assessing Methods of Medical Diagnosis with Special reference to X-Ray Techniques, 1947, Public Health Reports, pp1432-1449
 
@@ -551,7 +553,7 @@ class BinaryPairwiseMeasures(object):
 
         .. math::
 
-            FPR = \dfrac{FP}{\sharp \bar{Ref}}
+            FPR = \dfrac{FP}{|1-Ref|}
 
         Burke D, Brundage J, Redfield R., Measurement of the False positive rate in a screening Program for Human Immunodeficiency Virus Infections - 1988 - The New England Journal of Medicine 319 (15) 961-964
 
@@ -638,13 +640,12 @@ class BinaryPairwiseMeasures(object):
 
             CK = \dfrac{p_o - p_e}{1-p_e}
 
-        where
-
-        :math:`p_e = ` expected chance matching and :math:`p_o = `observed accuracy
+        where :math: `p_e = ` expected chance matching and :math: `p_o = `observed accuracy
 
         Cohen, J. A coefficient of agreement for nominal scales - Educational and Psychological Measurement (1960) 20 37-46
 
         :return: cohens_kappa
+
         """
         p_e = self.expected_matching_ck()
         p_o = self.accuracy()
@@ -743,10 +744,12 @@ class BinaryPairwiseMeasures(object):
         ..math::
 
             DSC = \dfrac{2TP}{2TP+FP+FN}
+
         
         This is also F:math:`{\\beta}` for :math:`{\\beta}`=1
 
         :return: dsc
+
         """
 
         numerator = 2 * self.tp()
@@ -880,9 +883,10 @@ class BinaryPairwiseMeasures(object):
 
         .. math::
 
-            IoR = \dfrac{\vert \text{Pred} \bigcap \text{Ref} \vert}{\vert Ref \vert}
+            IoR = \dfrac{| \text{Pred} \cap \text{Ref} |}{| Ref |}
 
         :return: IoR
+
         """
         if self.flag_empty_ref:
             warnings.warn("IoR not defined - Empty reference")
@@ -900,9 +904,10 @@ class BinaryPairwiseMeasures(object):
 
         .. math::
 
-            IoU = \dfrac{\vert Pred \bigcap Ref\vert}{\vert Pred \bigcup Ref \vert}
+            IoU = \dfrac{|Pred \cap Ref|}{| Pred \cup Ref |}
 
         :return: IoU
+
         """
         if self.flag_empty_pred and self.flag_empty_ref:
             warnings.warn("IoU not defined - Both reference and prediction are empty")
@@ -915,8 +920,10 @@ class BinaryPairwiseMeasures(object):
         This function calculates the euclidean distance between the centres
         of mass of the reference and prediction.
 
+        
         :return: Euclidean distance between centre of mass when reference and prediction not empty
         -1 otherwise
+
         """
         
         if self.flag_empty_pred or self.flag_empty_ref:
@@ -981,9 +988,10 @@ class BinaryPairwiseMeasures(object):
 
         .. math::
 
-            AVDR = \dfrac{\vert Pred - Ref\vert}{\vert Ref \vert} 
+            AVDR = \dfrac{| Pred - Ref|}{| Ref |} 
 
         :return: avdr
+
         """
         if self.n_pos_ref() == 0:
             warnings.warn('Empty reference - absolute volume difference ratio not defined')
@@ -1082,6 +1090,7 @@ Pattern Recognition. 15334–15342.
 
         where :math:A_d are the pixels of A within a distance d of the boundary
         :return: boundary_iou
+
         """
         if "boundary_dist" in self.dict_args.keys():
             distance = self.dict_args["boundary_dist"]
@@ -1180,6 +1189,7 @@ Pattern Recognition. 15334–15342.
 
         :return: hausdorff distance and average symmetric distance, hausdorff distance at perc
         and masd
+
         """
         if "hd_perc" in self.dict_args.keys():
             perc = self.dict_args["hd_perc"]
@@ -1223,7 +1233,7 @@ Pattern Recognition. 15334–15342.
 
         .. math::
 
-            ASSD(A,B) = \dfrac{\sum_{a\inA}d(a,B) + \sum_{b\inB}d(b,A)}{|A|+ |B|}
+            ASSD(A,B) = \dfrac{\sum_{a\in A}d(a,B) + \sum_{b\in B}d(b,A)}{|A|+ |B|}
 
         Heimann, T., et al. (2009), Comparison and evaluation of methods for liver segmentation from CT datasets. IEEE Trans Med Imaging. 28(8): p. 1251-65.
         Varduhi Yeghiazaryan and Irina Voiculescu. An overview of current evaluation methods used in medical image segmentation. Department of Computer Science, University of Oxford, 2015.
@@ -1242,9 +1252,11 @@ Pattern Recognition. 15334–15342.
 
         .. math::
 
-            MASD(A,B) = \dfrac{1}{2}\left(\dfrac{\sum_{a\in A}d(a,B)}{|A|} + \dfrac{\sum_{b\inB}d(b,A)}{|B|})
+            MASD(A,B) = \dfrac{1}{2}(\dfrac{\sum_{a\in A}d(a,B)}{|A|} + \dfrac{\sum_{b\in B}d(b,A)}{|B|})
         
+            
         :return: masd
+
         """
         masd = self.measured_distance()[3]
         return masd

test/test_metrics/test_calibration_metrics.py
@@ -120,12 +120,12 @@ def test_kernel_calibration_error():
     expected_median_heuristic = 0.90
     value_median = median_heuristic(pred)
     assert_allclose(value_median, expected_median_heuristic, atol = 0.01)
-    kernel_01 = np.exp(-np.sqrt(0.78)/value_median) * np.ones([3,3])
-    kernel_02 = np.exp(-np.sqrt(0.86)/value_median) * np.ones([3,3])
-    kernel_03 = np.exp(-np.sqrt(0.02)/value_median) * np.ones([3,3])
-    kernel_12 = np.exp(-np.sqrt(1.26)/value_median) * np.ones([3,3])
-    kernel_13 = np.exp(-np.sqrt(0.86)/value_median) * np.ones([3,3])
-    kernel_23 = np.exp(-np.sqrt(1.14)/value_median) * np.ones([3,3])
+    kernel_01 = np.exp(-np.sqrt(0.78)/value_median) * np.eye(3)
+    kernel_02 = np.exp(-np.sqrt(0.86)/value_median) * np.eye(3)
+    kernel_03 = np.exp(-np.sqrt(0.02)/value_median) * np.eye(3)
+    kernel_12 = np.exp(-np.sqrt(1.26)/value_median) * np.eye(3)
+    kernel_13 = np.exp(-np.sqrt(0.86)/value_median) * np.eye(3)
+    kernel_23 = np.exp(-np.sqrt(1.14)/value_median) * np.eye(3)
 
     vect_0 = np.asarray([-0.1, 0.4, -0.3])
     vect_1 = np.asarray([0.2, -0.1, -0.1])

test/test_metrics/test_pairwise_measures.py
@@ -5,8 +5,9 @@ from MetricsReloaded.processes.mixed_measures_processes import (
     MultiLabelLocSegPairwiseMeasure as MLIS,
 )
 import numpy as np
+
 from MetricsReloaded.utility.utils import one_hot_encode
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 from sklearn.metrics import cohen_kappa_score as cks
 from sklearn.metrics import matthews_corrcoef as mcc
 
@@ -279,6 +280,21 @@ def test_matthews_correlation_coefficient():
 
     assert_allclose(value_test, expected_mcc, atol=0.001)
     assert_allclose(value_test2, expected_mcc, atol=0.001)
+
+def test_confusion_matrix():
+    """
+    Taking Figure SN3.39 as inspiration
+    """
+
+    mask_ref = np.zeros([5,6])
+    mask_ref[0:5,0:4] = 1
+    mask_pred = np.zeros([5,6])
+    mask_pred[1:4,1:6]=1
+    mpm = MPM(np.reshape(mask_pred,[-1]),np.reshape(mask_ref,[-1]),[0,1])
+    cm_test = mpm.confusion_matrix()
+    cm = np.asarray([[4,11],[6,9]])
+    print(cm_test)
+    assert_array_equal(cm_test,cm)
 
 
 def test_ec3():